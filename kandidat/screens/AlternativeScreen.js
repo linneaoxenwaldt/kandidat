7658aeadd4e1ab85226b08b3474eb7c5eb0a99a8
Alice Lundberg <alicelundberg@nl144-107-187.student.uu.se>
2019-04-16 14:42:48 +0200

    alternativ

--- kandidat/screens/AlternativeScreen.js
+++ kandidat/screens/AlternativeScreen.js
@@ -7,226 +7,227 @@ import { ScrollView,
   View,
   Text,
   FlatList,
+  Alert,
   TextInput,
 } from 'react-native';
+import { ListItem } from 'react-native-elements';
 import { DrawerActions } from 'react-navigation';
 import Icon from "react-native-vector-icons/Ionicons";
-import { ExpoLinksView } from '@expo/samples';
-import AntIcon from "react-native-vector-icons/AntDesign";
+import data from '../data/engWord.json';
+
 
 
 export default class AlternativeScreen extends React.Component {
-
   constructor(props){
     super(props);
-    this.rows = [
-      {id: '0', text: 'Alternativ 1'},
-      {id: '1', text: 'Alternativ 2'},
-      {id: '2', text: 'Alternativ 3'},
-
-    ]
-    this.extractKey = ({id}) => id
-
-    this.state = {
-      arrayHolder: [],
-      textInput_Holder: ''
-    }
+    this.colors = ['#6ACCCB', '#94B4C1', '#8FBC8F', '#CBA3D5', '#689999']
+    const rows = [
+    {id: '0', text: 'Alternativ 1'},
+    {id: '1', text: 'Alternativ 2'},
+    {id: '2', text: 'Alternativ 3'},
+    {id: '3', text: 'Alternativ 4'},
+    {id: '4', text: 'Alternativ 5'},
+    {id: '5', text: 'Alternativ 6'},
+    {id: '6', text: 'Alternativ 7'},
+    {id: '7', text: 'Alternativ 8'},
+    {id: '8', text: 'Alternativ 9'},
+  ]
+  this.extractKey = ({id}) => id
+  this.state = {
+    rows: rows,
+    text: ''
   }
-
-  componentDidMount() {
-    this.setState({ arrayHolder: [...this.rows] })
-  }
-
-  joinData = () => {
-    this.rows.push({title : this.state.textInput_Holder});
-    this.setState({ arrayHolder: [...this.rows] })
-  }
-
-  FlatListItemSeparator = () => {
-    return (
-      <View
-      style={{
-        height: 2,
-        width: "100%",
-        backgroundColor: "white",
-      }}
-      />
-    );
-  }
-
-  GetItem(item) {
-    Alert.alert(item);
   }
 
   static navigationOptions = ({ navigation }) => {
-    return {
-      headerTitle: (
-        <Image source={require('../assets/images/100whitte.png')}/>),
-        headerStyle: {
-          backgroundColor: '#008080',
-          height: 70,
-          marginLeft: 10,
+      return {
+        headerTitle: (
+      <Image source={require('../assets/images/100whitte.png')}/>),
+      headerStyle: {
+        backgroundColor: '#008080',
+        height: 70,
+        marginLeft: 10,
         },
         headerRight: (
-          <TouchableOpacity
-          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-          >
-          <AntIcon name="delete" color="white" size={30} style={styles.trashIcon} />
-          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}
+>
+<Icon
+  name={Platform.OS === "ios" ? "ios-trash" : "md-trash"}
+  size={40}
+  color='#FFFFFF'/>
+</TouchableOpacity>
         ),
       };
     };
 
-
-    renderItem = ({item}) => {
-      return (
-        <TouchableOpacity onPress={() => alert('Remove')}
-        style={styles.fab}
-        style={styles.row}
-        underlayColor='#fff'>
-        <Text style={styles.readyMadeAlternativLabel}
-        style={styles.readyMadeAlternativLabel}>
-        {item.text}
-        </Text>
-        <Icon name={Platform.OS === "ios" ? "ios-remove-circle-outline" : "md-add-circle-outline"}
-        size={40}
-        style={styles.RemoveIcon}
-        />
-        </TouchableOpacity>
-      )
-    }
+    addNewAlternative() {
+     var index = this.state.rows.length
+     var newIDnum = parseInt(this.state.rows[index-1].id, 10) + 1
+     var newID = newIDnum.toString()
+     var newAlternative = {id: newID, text: this.state.text}
+     this.state.rows.push(newAlternative)
 
 
-    render() {
-      return (
-        <View style={styles.container}>
-        <Text style={styles.AlternativLabel}>Alternatives</Text>
-        <TouchableOpacity onPress={() => alert('Name of alternative')} style={styles.fab}
-        style={styles.createOwnAlternativ}
-        underlayColor='#fff'>
 
-        <TextInput
-        placeholder="Add new "
-        onChangeText={data => this.setState({ textInput_Holder: data })}
-        style={styles.ownAlternativText}
-        underlineColorAndroid='white'
-        />
-        </TouchableOpacity>
+     }
 
-        <TouchableOpacity onPress={this.joinData} activeOpacity={0.7}
-        style={styles.AddButton}>
-        <Text style={styles.ownAlternativText}>
+  deleteAlternative(delItem) {
+    this.setState(prevState => ({rows: prevState.rows.filter(item => item !== delItem) }));
+  }
+
+renderItem = ({item, index}) => {
+  var msg = `${data.sureMsg} ${item.text}?`
+  return (
+    <ListItem
+    containerStyle={{ backgroundColor: this.colors[index % this.colors.length]}}
+    titleStyle={{color: '#FFFFFF', fontSize: 20}}
+    title={item.text}
+    rightIcon = {<Icon name={Platform.OS === "ios" ? "ios-remove-circle-outline" : "md-add-circle-outline"}
+    size={40}
+    style={styles.RemoveIcon}
+    color= {'white'}
+    onPress={() => Alert.alert(
+  data.deleteAlternative,
+  msg,
+  [
+
+    {text: 'OK', onPress: () => this.deleteAlternative(item)},
+  ],
+  { cancelable: false })}/>}
+    />)
+}
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={styles.alternativeLabel}>{data.alternatives}</Text>
+      <TextInput
+      style={styles.textInput}
+      placeholder="Add new"
+      onChangeText={(text) => this.setState({text})}
+      />
+
+      <TouchableOpacity
+      style = {styles.saveButton}
+        onPress={() => this.addNewAlternative()}
+        >
         <Icon name={Platform.OS === "ios" ? "ios-add-circle-outline" : "md-add-circle-outline"}
         size={40}
         style={styles.RemoveIcon}
-        /></Text>
-        </TouchableOpacity>
-
-        <FlatList
-        data={this.state.arrayHolder}
-        width='100%'
-        extraData={this.state.arrayHolder}
-        keyExtractor={(index) => index.toString()}
-        ItemSeparatorComponent={this.FlatListItemSeparator}
-        renderItem={({ item }) => <Text style={styles.row} onPress={this.GetItem.bind(this, item.title)} > {item.title} </Text>}
+        color= {'white'}
         />
-        </View>
-      );
-    }
+       </TouchableOpacity>
+
+     <FlatList
+data={this.state.rows}
+renderItem={this.renderItem}
+keyExtractor={this.extractKey}
+/>
+
+<View>
+<TouchableOpacity style={styles.forwardArrow}
+onPress={() => this.props.navigation.navigate('AlternativeScreen')}>
+<Icon
+name={Platform.OS === "ios" ? "ios-arrow-forward" : "md-arrow-forward"}
+size={50}/>
+</TouchableOpacity>
+<TouchableOpacity style={styles.backArrow}
+onPress={() => this.props.navigation.navigate('NewVote')}>
+<Icon
+name={Platform.OS === "ios" ? "ios-arrow-back" : "md-arrow-back"}
+size={50}/>
+</TouchableOpacity>
+</View>
+      </View>
+
+
+
+    );
   }
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 15,
-      backgroundColor: '#FFFFFF',
-    },
-    contentContainer: {
-      paddingTop: 0,
-    },
-    AlternativContainer: {
-      alignItems: 'center',
-    },
-    AlternativLabel: {
-      fontSize: 30,
-      color: '#000000',
-      textAlign: 'center',
-      fontFamily: "Roboto-Light",
-    },
-    createOwnAlternativ: {
-      width: 300,
-      height: 70,
-      margin: 30,
-      paddingBottom: 20,
-      backgroundColor:'#BA55B3',
-      borderRadius:30,
-      borderWidth: 1,
-      borderColor: '#fff',
-      justifyContent: 'center'
-    },
-    ownAlternativText: {
-      fontFamily: "Roboto-Light",
-      color:'#fff',
-      fontSize: 25,
-      textAlign:'center',
-      paddingLeft : 1,
-      paddingRight : 1,
-    },
-    readyMadeAlternativContainer: {
-      alignItems: 'center',
-    },
-    readyMadeAlternativLabel: {
-      fontFamily: "Roboto-Light",
-      color: '#000000',
-      fontSize: 25,
-      color: 'white'
-    },
-    row: {
-      fontFamily: "Roboto-Light",
-      padding: 10,
-      marginBottom: 5,
-      backgroundColor: '#6ACCCB',
-      fontSize: 20,
-      color: 'white'
-    },
-    fab: {
-      position: 'absolute',
-      width: 76,
-      height: 76,
-      alignItems: 'center',
-      justifyContent: 'center',
-      right: 20,
-      bottom: 20,
-      backgroundColor: '#03A9F4',
-      borderRadius: 30,
-      elevation: 8
-    },
-    AddButton: {
-      width: 70,
-      height: 70,
-      margin: 10,
-      paddingBottom: 20,
-      backgroundColor:'#BA55B3',
-      borderRadius:50,
-      borderWidth: 1,
-      borderColor: '#fff',
-      justifyContent: 'center',
-
-
-    },
-    AddIcon: {
-      justifyContent: 'flex-end',
-      color: 'white',
-    },
-    RemoveIcon: {
-      justifyContent: 'flex-end',
-      color: 'white',
-    },
-
-    trashIcon: {
-      //flexBasis: 50,
-      justifyContent: 'flex-end',
-      color: '#FFFFFF',
-      marginLeft: 30,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#FFFFFF',
+    justifyContent:'center',
+  },
+  alternativeLabel: {
+    fontSize: 50,
+    color: '#000000',
+    textAlign: 'center',
+    fontFamily: "Roboto-Light",
+  },
+  addAlternative: {
+    justifyContent: 'center',
+    width: 350,
+    height: 70,
+    margin: 10,
+    padding: 10,
+    backgroundColor:'#BA55B3',
+    borderRadius:30,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  addAlternativeText: {
+    fontFamily: "Roboto-Light",
+    color:'#fff',
+    fontSize: 25,
+    textAlign:'center',
+    paddingLeft : 1,
+    paddingRight : 1,
+  },
+  alternativeContainer: {
+    marginTop: 40,
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+  alternativeText: {
+    fontFamily: "Roboto-Light",
+    color: '#000000',
+    fontSize: 25,
+  },
+  row: {
+  fontFamily: "Roboto-Light",
+  color: '#FFFFFF',
+  padding: 15,
+  marginBottom: 5,
+  fontSize: 20,
+  justifyContent: 'center',
+},
+textInput: {
+  backgroundColor: '#6BCDFD',
+  fontSize: 30,
+  textAlign:'center',
+  alignItems: 'center',
+  width: 350,
+  height: 70,
+  //backgroundColor: '#8FBC8F',
+  borderRadius: 30,
+  marginBottom: 10,
+  padding: 10,
+  color: '#FFFFFF',
+},
+saveButton: {
+  backgroundColor: "#BA55B3",
+  width: 70,
+  height: 70,
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 100,
+  marginBottom: 10,
+  marginTop: 50,
+},
+addIcon: {
+  color: "#FFFFFF",
+  fontSize: 30,
+  fontFamily: 'Roboto-Light',
+},
+forwardArrow: {
+  justifyContent: 'center',
+},
+backArrow: {
+  justifyContent: 'center',
+},
+});
