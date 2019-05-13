56ab7d860f2e61f334085d9c5a689cdc848af420
Ambellah <alicedahlqvist@live.se>
2019-05-13 13:39:29 +0200

    mer småfix

--- kandidat/screens/ResultScreen.js
+++ kandidat/screens/ResultScreen.js
@@ -55,221 +55,213 @@ export default class ResultScreen extends React.Component {
       this.getResult()
     }
 
-  getResult() {
-    var that = this;
-    var user = firebase.auth().currentUser;
-    var userID = user.uid;
-    var db = firebase.firestore();
-    var voteID = this.props.navigation.state.params.VoteID
-    var docRef = db.collection("Users").doc(userID).collection("Result").doc(voteID)
-docRef.collection("Alternatives").onSnapshot(function(querySnapshot) {
-  that.setState({result: []})
-    querySnapshot.forEach(function(doc) {
-      const altID = doc.id;
-      const name = doc.get('Name')
-      const votes = doc.get('Votes')
-    //  console.log(that.state.result)
-      that.setState(prevState => ({
-        result: [...prevState.result, {AltID: altID, Name: name, Votes: votes}]
-      }))
-      that.state.result.sort((a, b) => (a.Votes < b.Votes) ? 1 : -1)
-      that.setState({winner: that.state.result[0].Name})
-      if(that.state.result.length > 1){
-        that.setState({second: that.state.result[1].Name})
-      }
-      if(that.state.result.length > 2){
-          that.setState({third: that.state.result[2].Name})
-      }
-    })
-  })
-}
+    getResult() {
+      var that = this;
+      var user = firebase.auth().currentUser;
+      var userID = user.uid;
+      var db = firebase.firestore();
+      var voteID = this.props.navigation.state.params.VoteID
+      var docRef = db.collection("Users").doc(userID).collection("Result").doc(voteID)
+      docRef.collection("Alternatives").onSnapshot(function(querySnapshot) {
+        that.setState({result: []})
+        querySnapshot.forEach(function(doc) {
+          const altID = doc.id;
+          const name = doc.get('Name')
+          const votes = doc.get('Votes')
+          that.setState(prevState => ({
+            result: [...prevState.result, {AltID: altID, Name: name, Votes: votes}]
+          }))
+          that.state.result.sort((a, b) => (a.Votes < b.Votes) ? 1 : -1)
+          that.setState({winner: that.state.result[0].Name})
+          if(that.state.result.length > 1){
+            that.setState({second: that.state.result[1].Name})
+          }
+          if(that.state.result.length > 2){
+            that.setState({third: that.state.result[2].Name})
+          }
+        })
+      })
+    }
 
-saveResult() {
-  //var that = this;
-  var user = firebase.auth().currentUser;
-  var userID = user.uid;
-  var db = firebase.firestore();
-  var voteID = this.props.navigation.state.params.VoteID
-  var docRef = db.collection('Users').doc(userID).collection('Result').doc(voteID)
-  docRef.update({
-    Saved: true
-  })
-  
-}
+    saveResult() {
+      var user = firebase.auth().currentUser;
+      var userID = user.uid;
+      var db = firebase.firestore();
+      var voteID = this.props.navigation.state.params.VoteID
+      var docRef = db.collection('Users').doc(userID).collection('Result').doc(voteID)
+      docRef.update({
+        Saved: true
+      })
 
-deleteResult() {
-  var that = this;
-  var user = firebase.auth().currentUser;
-  var userID = user.uid;
-  var db = firebase.firestore();
-  var voteID = this.props.navigation.state.params.VoteID
-  var docRef = db.collection('Users').doc(userID).collection('Result').doc(voteID)
-  docRef.collection('Alternatives').get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-          // doc.data() is never undefined for query doc snapshots
+    }
+
+    deleteResult() {
+      var that = this;
+      var user = firebase.auth().currentUser;
+      var userID = user.uid;
+      var db = firebase.firestore();
+      var voteID = this.props.navigation.state.params.VoteID
+      var docRef = db.collection('Users').doc(userID).collection('Result').doc(voteID)
+      docRef.collection('Alternatives').get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
           const id = doc.id
           docRef.collection('Alternatives').doc(id).delete().then(function() {
             console.log("delete alternatives ");
           }).catch(function(error) {
             console.error("delete alternatives ", error);
           });
+        });
       });
-  });
-  docRef.delete().then(function() {
-  console.log("delete vote " + voteID);
-  }).catch(function(error) {
-  console.error("delete vote ", error);
-  });
-}
-
-      render() {
-        return (
-          <View style={styles.container}>
-          <Image source={require('../assets/images/medal.png')} style={styles.medalPic}/>
-          <Text style={styles.resultLabel}>{data.result}</Text>
-          <View style={styles.firstContainer}>
-          <Text style={styles.containText}>{this.state.winner}</Text>
-          <View style={styles.trophyIcon}>
-          <Icon name={Platform.OS === "ios" ? "ios-trophy" : "md-create"}
-          size={40}
-          color='#daa520'/>
-          </View>
-          </View>
-
-          <View style={styles.secondContainer}>
-          <Text style={styles.containText}>{this.state.second}</Text>
-          <View style={styles.trophyIcon}>
-          <Icon name={Platform.OS === "ios" ? "ios-trophy" : "md-create"}
-          size={40}
-          color='#c0c0c0'/>
-          </View>
-          </View>
-
-          <View style={styles.thirdContainer}>
-          <Text style={styles.containText}>{this.state.third}</Text>
-          <View style={styles.trophyIcon}>
-          <Icon name={Platform.OS === "ios" ? "ios-trophy" : "md-create"}
-          size={40}
-          color='#a0522d'/>
-          </View>
-          </View>
-
-          <View style = {styles.buttonContainer}>
-
-          <TouchableOpacity
-          style = {styles.saveResult}
-          underlayColor='#fff'
-            onPress={() => this.saveResult()}>
-          <Text style= {styles.saveResultText}>{data.delete} </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-          style = {styles.saveResult}
-          underlayColor='#fff'
-          onPress={() => this.deleteResult()}>
-          <Text style= {styles.saveResultText}>{data.save}{data.result}</Text>
-          </TouchableOpacity>
-          </View>
-          </View>
-        );
-      }
+      docRef.delete().then(function() {
+      }).catch(function(error) {
+        console.error("delete vote ", error);
+      });
     }
 
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        paddingTop: 15,
-        backgroundColor: '#FFFFFF',
-        alignItems: 'center',
-      },
-      medalPic: {
-        height: 120,
-        width: 120,
-        //borderRadius: 100,
-        //borderWidth: 2,
-        //borderColor: 'white',
-      },
-      resultLabel:{
-        fontFamily: 'Roboto-Light',
-        color: 'black',
-        fontSize: 40,
-        padding: 20,
-      },
-      containText: {
-        textAlign: 'center',
-        flexGrow: 1,
-        fontFamily: 'Roboto-Light',
-        color: '#FFFFFF',
-        fontSize: 25,
-        alignSelf: 'center',
-      },
-      firstContainer: {
-        flexDirection:'row',
-        width: 350,
-        height: 70,
-        backgroundColor: '#8FBC8F',
-        justifyContent: 'center',
-        borderRadius: 30,
-        margin: 5,
-        padding: 10,
-      },
-      secondContainer: {
-        flexDirection:'row',
-        width: 350,
-        height: 70,
-        backgroundColor: '#6ACCCB',
-        justifyContent: 'center',
-        borderRadius: 30,
-        margin: 5,
-        padding: 10,
-      },
-      thirdContainer: {
-        flexDirection:'row',
-        width: 350,
-        height: 70,
-        backgroundColor: '#94B4C1',
-        justifyContent: 'center',
-        borderRadius: 30,
-        margin: 5,
-        padding: 10,
-      },
-      trophyIcon: {
-        justifyContent:'center',
-        alignItems:'center',
-        alignSelf: 'center',
-        textAlign:'center',
-        backgroundColor: 'white',
-        borderRadius:100,
-        alignSelf: 'center',
-        color: '#daa520',
-        paddingRight: 0,
-        height: 50,
-        width: 50,
-        borderWidth: 2,
-        borderColor: 'white'
-      },
-      buttonContainer:{
-        flexDirection:'row',
-        justifyContent: 'space-between',
-        marginLeft: 10,
-        marginRight: 10,
-      },
-      saveResult: {
-        justifyContent: 'center',
-        width: 130,
-        height: 70,
-        margin: 10,
-        padding: 10,
-        //marginTop:100,
-        backgroundColor:'#BA55B3',
-        borderRadius:20,
-        borderWidth: 1,
-        borderColor: '#fff'
-      },
-      saveResultText:{
-        fontSize:20,
-        textAlign:'center',
-        alignItems:'center',
-        color: 'white',
-      },
-    });
+    render() {
+      return (
+        <View style={styles.container}>
+        <Image source={require('../assets/images/medal.png')} style={styles.medalPic}/>
+        <Text style={styles.resultLabel}>{data.results}</Text>
+        <View style={styles.firstContainer}>
+        <Text style={styles.containText}>{this.state.winner}</Text>
+        <View style={styles.trophyIcon}>
+        <Icon name={Platform.OS === "ios" ? "ios-trophy" : "md-create"}
+        size={40}
+        color='#daa520'/>
+        </View>
+        </View>
+
+        <View style={styles.secondContainer}>
+        <Text style={styles.containText}>{this.state.second}</Text>
+        <View style={styles.trophyIcon}>
+        <Icon name={Platform.OS === "ios" ? "ios-trophy" : "md-create"}
+        size={40}
+        color='#c0c0c0'/>
+        </View>
+        </View>
+
+        <View style={styles.thirdContainer}>
+        <Text style={styles.containText}>{this.state.third}</Text>
+        <View style={styles.trophyIcon}>
+        <Icon name={Platform.OS === "ios" ? "ios-trophy" : "md-create"}
+        size={40}
+        color='#a0522d'/>
+        </View>
+        </View>
+
+        <View style = {styles.buttonContainer}>
+
+        <TouchableOpacity
+        style = {styles.saveResult}
+        underlayColor='#fff'
+        onPress={() => this.saveResult()}>
+        <Text style= {styles.saveResultText}>{data.delete} </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        style = {styles.saveResult}
+        underlayColor='#fff'
+        onPress={() => this.deleteResult()}>
+        <Text style= {styles.saveResultText}>{data.save}{data.result}</Text>
+        </TouchableOpacity>
+        </View>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 15,
+      backgroundColor: '#FFFFFF',
+      alignItems: 'center',
+    },
+    medalPic: {
+      height: 120,
+      width: 120,
+    },
+    resultLabel:{
+      fontFamily: 'Roboto-Light',
+      color: '#000',
+      fontSize: 40,
+      padding: 20,
+    },
+    containText: {
+      textAlign: 'center',
+      flexGrow: 1,
+      fontFamily: 'Roboto-Light',
+      color: '#FFFFFF',
+      fontSize: 25,
+      alignSelf: 'center',
+    },
+    firstContainer: {
+      flexDirection:'row',
+      width: 350,
+      height: 70,
+      backgroundColor: '#8FBC8F',
+      justifyContent: 'center',
+      borderRadius: 30,
+      margin: 5,
+      padding: 10,
+    },
+    secondContainer: {
+      flexDirection:'row',
+      width: 350,
+      height: 70,
+      backgroundColor: '#6ACCCB',
+      justifyContent: 'center',
+      borderRadius: 30,
+      margin: 5,
+      padding: 10,
+    },
+    thirdContainer: {
+      flexDirection:'row',
+      width: 350,
+      height: 70,
+      backgroundColor: '#94B4C1',
+      justifyContent: 'center',
+      borderRadius: 30,
+      margin: 5,
+      padding: 10,
+    },
+    trophyIcon: {
+      justifyContent:'center',
+      alignItems:'center',
+      alignSelf: 'center',
+      textAlign:'center',
+      backgroundColor: 'white',
+      borderRadius:100,
+      alignSelf: 'center',
+      color: '#daa520',
+      paddingRight: 0,
+      height: 50,
+      width: 50,
+      borderWidth: 2,
+      borderColor: 'white'
+    },
+    buttonContainer:{
+      flexDirection:'row',
+      justifyContent: 'space-between',
+      marginLeft: 10,
+      marginRight: 10,
+    },
+    saveResult: {
+      justifyContent: 'center',
+      width: 130,
+      height: 70,
+      margin: 10,
+      padding: 10,
+      backgroundColor:'#BA55B3',
+      borderRadius:20,
+      borderWidth: 1,
+      borderColor: '#fff'
+    },
+    saveResultText:{
+      fontSize:20,
+      textAlign:'center',
+      alignItems:'center',
+      color: 'white',
+    },
+  });
